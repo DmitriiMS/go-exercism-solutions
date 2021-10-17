4f70da10bc48d44e68db25f8772008fa6e6382f0
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-10-17 07:42:33 +0300

    interest-is-interesting: working solution with passing tests

--- interest-is-interesting/savings_account.go
+++ interest-is-interesting/savings_account.go
@@ -1,21 +1,45 @@
 package savings
 
+import (
+	"math"
+)
+
 // InterestRate returns the interest rate for the provided balance.
 func InterestRate(balance float64) float32 {
-	panic("Please implement the InterestRate function")
+	switch {
+	case balance < 0.0:
+		return -3.213
+	case balance >= 0 && balance < 1000:
+		return 0.5
+	case balance >= 1000 && balance < 5000:
+		return 1.621
+	case balance >= 5000:
+		return 2.475
+	default:
+		return 0.0 // just to return someting for any case, even if it's unreachable in given confitions
+	}
 }
 
 // Interest calculates the interest for the provided balance.
 func Interest(balance float64) float64 {
-	panic("Please implement the Interest function")
+	rate := InterestRate(balance)
+	//math.Abs() to get the right sign
+	//float64(rate) -- works fine here, but 64 being more precise, the numbers can differ a little
+	//in a more complex case it can lead to problems
+	return (math.Abs(balance) / 100.0) * float64(rate)
 }
 
 // AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
 func AnnualBalanceUpdate(balance float64) float64 {
-	panic("Please implement the AnnualBalanceUpdate function")
+	interest := Interest(balance)
+	return balance + interest
 }
 
 // YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance:
 func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
-	panic("Please implement the YearsBeforeDesiredBalance function")
+	years := 0 // declared here so it is availiable outside the loop
+	for currentBalance := balance; currentBalance <= targetBalance; years++ {
+		currentBalance = AnnualBalanceUpdate(currentBalance)
+	}
+	return years
 }
