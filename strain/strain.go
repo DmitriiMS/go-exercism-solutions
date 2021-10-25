c35f132af6213210ea9f82b01b757335da1f03e8
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-10-25 11:00:39 +0300

    strain: solution

--- strain/strain.go
+++ strain/strain.go
@@ -1,1 +1,51 @@
 package strain
+
+type Ints []int
+type Lists [][]int
+type Strings []string
+
+func (ints *Ints) Keep(f func(int) bool) Ints {
+	if *ints == nil {
+		return nil
+	}
+	newInts := []int{}
+	for _, i := range *ints {
+		if f(i) {
+			newInts = append(newInts, i)
+		}
+	}
+	return newInts
+}
+
+func (ints *Ints) Discard(f func(int) bool) Ints {
+	if *ints == nil {
+		return nil
+	}
+	newInts := []int{}
+	for _, i := range *ints {
+		if !f(i) {
+			newInts = append(newInts, i)
+		}
+	}
+	return newInts
+}
+
+func (lists *Lists) Keep(f func([]int) bool) Lists {
+	newLists := [][]int{}
+	for _, l := range *lists {
+		if f(l) {
+			newLists = append(newLists, l)
+		}
+	}
+	return newLists
+}
+
+func (strings *Strings) Keep(f func(string) bool) Strings {
+	newStrings := []string{}
+	for _, s := range *strings {
+		if f(s) {
+			newStrings = append(newStrings, s)
+		}
+	}
+	return newStrings
+}
