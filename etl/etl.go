6d65b638254e9576d9c7b78bceca3aaafbe33bd0
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-10-25 11:54:13 +0300

    etl: simplified solution, got rid of useless checks

--- etl/etl.go
+++ etl/etl.go
@@ -11,9 +11,7 @@ func Transform(in old) new {
 	var transformed = make(new)
 	for k, v := range in {
 		for _, letter := range v {
-			if _, ok := transformed[strings.ToLower(letter)]; !ok {
-				transformed[strings.ToLower(letter)] = k
-			}
+			transformed[strings.ToLower(letter)] = k
 		}
 	}
 	return transformed
