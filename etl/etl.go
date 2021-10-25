e34814013029974888c8c9b752d10880f5b8a851
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-10-25 11:47:35 +0300

    etl: solution

--- etl/etl.go
+++ etl/etl.go
@@ -1,1 +1,20 @@
 package etl
+
+import (
+	"strings"
+)
+
+type old map[int][]string
+type new map[string]int
+
+func Transform(in old) new {
+	var transformed = make(new)
+	for k, v := range in {
+		for _, letter := range v {
+			if _, ok := transformed[strings.ToLower(letter)]; !ok {
+				transformed[strings.ToLower(letter)] = k
+			}
+		}
+	}
+	return transformed
+}
