6a00b9b18cc71b5ce664025e95f444b6d4024381
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-10-26 09:31:15 +0300

    pangram: slightly better solution -- trimmed the fat off a slice

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -7,18 +7,18 @@ import (
 
 //IsPangram takes a sentence and validates whether or not it is a pangram
 func IsPangram(in string) bool {
-	var codes []int = make([]int, 'Z'+1) // create an int slice with indecies from 0 to 90
-	for i := 'A'; i <= 'Z'; i++ {        //uppercase letters from A to Z have ascii codes from 65 to 90
-		codes[i] = 1 //fill indecies in this range with ones to represent that those letters are not yet encountered
+	var codes []int = make([]int, 26) // create an int slice for all letters
+	for i := 0; i <= 25; i++ {
+		codes[i] = 1 //fill with ones, 1 means letter was not encountered, 0 -- was encountered
 	}
 	for _, letter := range in {
-		uppercase := unicode.ToUpper(letter)      //take each rune in sentence try to set it to upper case
-		if uppercase >= 'A' && uppercase <= 'Z' { //runes are just codes, in case of ascii -- ascii codes, here is a chek that it's a letter
-			codes[uppercase] = 0 // if it's a letter, set this code to 0: already encountered
+		uppercase := unicode.ToUpper(letter)         //take each rune in sentence and try to set it to upper case
+		if uppercase-65 >= 0 && uppercase-65 <= 25 { //runes are just codes, in case of uppercase ascii letters -- codes from 65 to 90; substracting 65 gives a range of 0 to 25
+			codes[uppercase-65] = 0 // if in range, set this code to 0 -- we met this particular letter
 		}
 	}
 	//now run over the slice and check that every item is 0, if not, given sentence lacked some letters, in which case return false
-	for i := 0; i <= 'Z'; i++ {
+	for i := 0; i <= 25; i++ {
 		if codes[i] != 0 {
 			return false
 		}
