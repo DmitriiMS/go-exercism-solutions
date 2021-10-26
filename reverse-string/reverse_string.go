400b28946c14b1560045b4271602d9af1fdb4467
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-10-26 09:52:08 +0300

    revers_string: slightly better solution

--- reverse-string/reverse_string.go
+++ reverse-string/reverse_string.go
@@ -1,8 +1,10 @@
 package reverse
 
-func Reverse(input string) (output string) {
-	for _, letter := range input {
-		output = string(letter) + output
+//not an original solution by me, but I wanted to type it out and drill into my brain that I can easily swap slice lements around
+func Reverse(input string) string {
+	sliced := []rune(input)
+	for i, j := 0, len(sliced)-1; i < j; i, j = i+1, j-1 {
+		sliced[i], sliced[j] = sliced[j], sliced[i]
 	}
-	return
+	return string(sliced)
 }
