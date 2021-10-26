d4172766f54aef1076318110b1a7396916104b68
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-10-26 12:42:20 +0300

    anagram: another solution, this tine using sort.Strings() from standard library. Shorter, but works a bit slower than custom type one.

--- anagram/anagram.go
+++ anagram/anagram.go
@@ -6,13 +6,6 @@ import (
 	"strings"
 )
 
-type ByRune []rune //custom type for methods
-
-//methods needed to use sort function.
-func (r *ByRune) Len() int           { return len(*r) }
-func (r *ByRune) Swap(i, j int)      { (*r)[i], (*r)[j] = (*r)[j], (*r)[i] }
-func (r *ByRune) Less(i, j int) bool { return (*r)[i] < (*r)[j] }
-
 //Detect detects anagrams
 func Detect(word string, candidates []string) (result []string) {
 	upWord := strings.ToUpper(word)               //uppercase the target word because anagram search should be case insensitive
@@ -20,10 +13,10 @@ func Detect(word string, candidates []string) (result []string) {
 	if len(candidates) == 0 {                     //if slice is empty after the validation, return empty slice
 		return result
 	}
-	sortedWord := SortStringByCharacter(upWord) //sort the target word by characters
+	sortedWord := SortString(upWord) //sort the target word by characters
 	//uppercase each candidate, sort it by letters and if it equals to the target word, add it to the resulting slice
 	for _, candidate := range candidates {
-		if SortStringByCharacter(strings.ToUpper(candidate)) != sortedWord {
+		if SortString(strings.ToUpper(candidate)) != sortedWord {
 			continue
 		}
 		result = append(result, candidate)
@@ -43,9 +36,9 @@ func checkStrings(word string, candidates []string) []string {
 	return candidates
 }
 
-//SortStringByCharacter converts string to rune slice, sorts it and returns it as a string
-func SortStringByCharacter(s string) string {
-	var r ByRune = []rune(s)
-	sort.Sort(&r)
-	return string(r)
+//SortString sorts string by letters
+func SortString(s string) string {
+	letters := strings.Split(s, "")
+	sort.Strings(letters)
+	return strings.Join(letters, "")
 }
