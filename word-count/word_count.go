cc547fd5134cb43390c170de4790e4b2acca6bf9
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-11-01 15:23:05 +0300

    word_count: solution

--- word-count/word_count.go
+++ word-count/word_count.go
@@ -1,7 +1,21 @@
 package wordcount
 
+import (
+	"regexp"
+	"strings"
+)
+
 type Frequency map[string]int
 
 func WordCount(input string) Frequency {
-
+	freq := make(Frequency)
+	punctuation := regexp.MustCompile(`[!?.,_\-:;&@$%^&\n\t]`)
+	depunctuated := punctuation.ReplaceAll([]byte(input), []byte(" "))
+	sliced := strings.Split(string(depunctuated), " ")
+	for _, word := range sliced {
+		if len(word) > 0 {
+			freq[strings.ToLower(strings.Trim(word, "'"))] += 1
+		}
+	}
+	return freq
 }
