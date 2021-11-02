e632c5e3a36ca7525a7e5624523003c0bde369be
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-11-02 11:55:29 +0300

    word_count: more optimal solution without regex,             using FieldsFunc from strings instead.

--- word-count/word_count.go
+++ word-count/word_count.go
@@ -1,16 +1,18 @@
 package wordcount
 
 import (
-	"regexp"
 	"strings"
+	"unicode"
 )
 
 type Frequency map[string]int
 
 func WordCount(input string) Frequency {
 	freq := make(Frequency)
-	wordFinder := regexp.MustCompile(`[\w\d']+`)
-	words := wordFinder.FindAll([]byte(input), -1)
+	splitter := func(c rune) bool {
+		return !unicode.IsLetter(c) && !unicode.IsNumber(c) && (c) != '\''
+	}
+	words := strings.FieldsFunc(input, splitter)
 	for _, word := range words {
 		if len(word) > 0 {
 			freq[strings.ToLower(strings.Trim(string(word), "'"))] += 1
