c58ea71787eac16d74cfa3952b73b25d549f5e1a
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-11-02 12:26:03 +0300

    isbn_verifier: solution

--- isbn-verifier/isbn_verifier.go
+++ isbn-verifier/isbn_verifier.go
@@ -1,5 +1,26 @@
 package isbn
 
+import (
+	"unicode"
+)
+
 func IsValidISBN(isbn string) bool {
-	panic("Please implement the IsValidISBN function")
+	if len(isbn) == 0 {
+		return false
+	}
+	sum, i := 0, 10
+	for _, c := range isbn {
+		if c == 'X' && i == 1 {
+			sum += 10
+			i = 0
+		}
+		if unicode.IsDigit(c) {
+			sum += int(c-'0') * i
+			i--
+		}
+	}
+	if i != 0 || sum%11 != 0 {
+		return false
+	}
+	return true
 }
