fa26833c5f626424dd1c7b282c48fcbd3962a672
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-11-02 11:49:19 +0300

    word_count: solution where I look for words, not separators

--- word-count/word_count.go
+++ word-count/word_count.go
@@ -9,12 +9,11 @@ type Frequency map[string]int
 
 func WordCount(input string) Frequency {
 	freq := make(Frequency)
-	punctuation := regexp.MustCompile(`[!?.,_\-:;&@$%^&\n\t]`)
-	depunctuated := punctuation.ReplaceAll([]byte(input), []byte(" "))
-	sliced := strings.Split(string(depunctuated), " ")
-	for _, word := range sliced {
+	wordFinder := regexp.MustCompile(`[\w\d']+`)
+	words := wordFinder.FindAll([]byte(input), -1)
+	for _, word := range words {
 		if len(word) > 0 {
-			freq[strings.ToLower(strings.Trim(word, "'"))] += 1
+			freq[strings.ToLower(strings.Trim(string(word), "'"))] += 1
 		}
 	}
 	return freq
