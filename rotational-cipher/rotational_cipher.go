4dc2fe4936829cd858288d22ce688dd6c2cf0f93
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-11-05 10:23:02 +0300

    rotational_cipher: solution

--- rotational-cipher/rotational_cipher.go
+++ rotational-cipher/rotational_cipher.go
@@ -1,5 +1,19 @@
 package rotationalcipher
 
+import "unicode"
+
 func RotationalCipher(plain string, shiftKey int) string {
-	panic("Please implement the RotationalCipher function")
+	newstr := make([]rune, len(plain))
+	for i, c := range plain {
+		if unicode.IsLetter(c) {
+			if unicode.ToLower(c)-'a'+rune(shiftKey) >= 26 {
+				newstr[i] = c + (rune(shiftKey) - 26)
+				continue
+			}
+			newstr[i] = c + rune(shiftKey)
+			continue
+		}
+		newstr[i] = c
+	}
+	return string(newstr)
 }
