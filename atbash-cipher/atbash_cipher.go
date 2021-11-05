dcc94349d2bcf31778f982436b465f7c352b607e
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-11-05 11:30:52 +0300

    atbash_cipher: solution

--- atbash-cipher/atbash_cipher.go
+++ atbash-cipher/atbash_cipher.go
@@ -1,5 +1,30 @@
 package atbash
 
+import "unicode"
+
+type message struct {
+	str string
+	i   int
+}
+
 func Atbash(s string) string {
-	panic("Please implement the Atbash function")
+	encoded := message{"", 0}
+	for _, c := range s {
+		if unicode.IsLetter(c) {
+			swapped := 'a' + 25 - (unicode.ToLower(c) - 'a')
+			encoded.addChar(swapped)
+		} else if unicode.IsDigit(c) {
+			encoded.addChar(c)
+		}
+	}
+	return encoded.str
+}
+
+func (m *message) addChar(char rune) {
+	if m.i == 5 {
+		m.str += " "
+		m.i = 0
+	}
+	m.str += string(char)
+	m.i++
 }
