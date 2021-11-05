ec313ba2d128b6a357a01c4934827855210e01a9
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-11-05 10:42:22 +0300

    rotational_cipher: slightly better solution, based on one from the community

--- rotational-cipher/rotational_cipher.go
+++ rotational-cipher/rotational_cipher.go
@@ -1,19 +1,16 @@
 package rotationalcipher
 
-import "unicode"
-
 func RotationalCipher(plain string, shiftKey int) string {
 	newstr := make([]rune, len(plain))
 	for i, c := range plain {
-		if unicode.IsLetter(c) {
-			if unicode.ToLower(c)-'a'+rune(shiftKey) >= 26 {
-				newstr[i] = c + (rune(shiftKey) - 26)
-				continue
-			}
-			newstr[i] = c + rune(shiftKey)
-			continue
+		switch {
+		case c >= 'a' && c <= 'z':
+			newstr[i] = 'a' + ((c - 'a' + rune(shiftKey)) % 26)
+		case c >= 'A' && c <= 'Z':
+			newstr[i] = 'A' + ((c - 'A' + rune(shiftKey)) % 26)
+		default:
+			newstr[i] = c
 		}
-		newstr[i] = c
 	}
 	return string(newstr)
 }
